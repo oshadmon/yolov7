d10caa9fba92ebca46faa47444fe9469d72dcc93
Ori Shadmon <oshadmon@gmail.com>
2024-05-28 09:13:03 -0700

    camera actions
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- camera_action.py
+++ camera_action.py
@@ -4,27 +4,38 @@ import datetime
 import os
 import time
 import threading
+import json
 
 ROOT_PATH = os.path.dirname(__file__)
 BLOBS_DIR = os.path.join(ROOT_PATH, 'blobs')
-if not os.path.isdir(BLOBS_DIR):
-    os.makedirs(BLOBS_DIR)
+os.makedirs(BLOBS_DIR, exist_ok=True)
+DATA_FILE = os.path.join(BLOBS_DIR, 'raw_records.json')
+open(DATA_FILE, 'a').close()
+def write_file(metadata):
+    # Here you would implement your logic to write metadata to a database or file
+    with open(DATA_FILE, 'a') as f:
+        f.write(json.dumps(metadata) + "\n")
 
-def get_default_camera_id():
-    camera_id = None
-    for i in range(10):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            cap.release()
-            camera_id = i
-            break
-
-    if camera_id is None:
-        raise Exception("Error: Could not find an available camera.")
-    return camera_id
 
 class VideoRecorder:
-    def __init__(self, camera_id:int=0, width:float=640, height:float=480, wait_time:int=60):
+    def __init__(self, camera_id:int=0, width:float=640, height:float=480, wait_time:float=60):
+        """
+        Record video for user
+        :args:
+            camera_id:int - Camera ID
+            width:float -  Image width size
+            height:float - Image height size
+            wait_time:float - how long each video will be
+        :params:
+            self.is_running:bool
+            self.cap:cv2.VideoCapture - VideoCapture
+            self.video_writer:cv2.VideoWriter
+            self.start_time:float - epoch timestamp gets updated every time we create a new video
+            self.camera_id:int - Camera ID
+            self.width:float -  Image width size
+            self.height:float - Image height size
+            self.wait_time:float - how long each video will be
+        """
         self.camera_id = camera_id
         self.width = width
         self.height = height
@@ -44,7 +55,13 @@ class VideoRecorder:
             raise Exception(f"Failed to start video capture with camera {self.camera_id} (Error: {error})")
         return cap
 
-    def __set_cap_size(self, height:float=None, width:float=None):
+    def __set_cap_size(self, height=None, width=None):
+        """
+        Image capture size
+        :args:
+            width:int -  Image width size
+            height:int - Image height size
+        """
         if height is None:
             height = self.height
         if width is None:
@@ -54,88 +71,144 @@ class VideoRecorder:
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     def __create_video_writer(self):
-        self.filename = os.path.join(BLOBS_DIR, f'{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.mp4')
+        """
+        Create video and store in file each time
+        :params:
+            self.filename - file to store video in
+        """
+        self.filename = os.path.join(BLOBS_DIR, f'{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_$f")}.mp4')
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         video_writer = cv2.VideoWriter(self.filename, fourcc, 20.0, (int(self.width), int(self.height)))
         return video_writer
 
     def start_recording(self):
+        """
+        Start recording
+        """
         self.__set_cap_size()
-
         self.is_running = True
         threading.Thread(target=self.__record).start()
 
     def __record(self):
+        """
+        Actual recording process  - once recording is done, write video info into a JSON file
+        """
         while self.is_running:
             ret, frame = self.cap.read()
             if not ret:
                 print("Error: Could not read frame.")
                 self.is_running = False
                 break
-                # continue  # Instead of breaking, we continue to try reading the next frame
 
             current_time = time.time()
-            if current_time - self.start_time >= self.wait_time:  # Check if wait_time seconds have passed
-                self.video_writer.release()  # Release the current video writer
-                self.video_writer = self.__create_video_writer()  # Create a new video writer
-                # write to database
-                print({"file_path": self.filename, "start_time": datetime.datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
-                       "end_time": datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
-                       "duration": round(current_time - self.start_time, 2)})
-                self.start_time = current_time  # Reset the start time
+            if current_time - self.start_time >= self.wait_time:
+                self.video_writer.release()
+                self.video_writer = self.__create_video_writer()
+                write_file({
+                    "file_path": self.filename,
+                    "start_time": datetime.datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
+                    "end_time": datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
+                    "duration": round(current_time - self.start_time, 2)
+                })
+                self.start_time = current_time
 
-            self.video_writer.write(frame)  # Write the frame to the video file
-
+            self.video_writer.write(frame)
 
     def stop_recording(self):
+        """
+        Stop recording
+        """
         self.is_running = False
         self.cap.release()
-        self.video_writer.release()  # Release the video writer
+        self.video_writer.release()
 
-    def display_feed(self, height:float=None, width:float=None):
-        self.__set_cap_size(height=height, width=width)
+    # def display_feed(self, height=None, width=None):
+    #     self.__set_cap_size(height=height, width=width)
+    #     while True:
+    #         ret, frame = self.cap.read()
+    #         if not ret:
+    #             print("Error: Could not read frame.")
+    #             continue
+    #
+    #         cv2.imshow('Video Feed', frame)
+    #         if cv2.waitKey(1) & 0xFF == ord('q'):
+    #             break
+    #     cv2.destroyAllWindows()
+
+
+def video_reader(file_path:str):
+    """
+    Read video file from cv2
+    """
+    try:
+        full_path = os.path.expandvars(os.path.expandvars(file_path))
+    except Exception as error:
+        print(f"Failed to locate file {file_path} (Error: {error})")
+        exit(1)
+    else:
+        if not os.path.isfile(full_path):
+            print(f"Failed to locate file {file_path}")
+            exit(1)
+
+    try:
+        cap = cv2.VideoCapture(full_path)
+    except Exception as error:
+        print(f"Failed to open video file {file_path} (Error: {error})")
+        exit(1)
+    else:
+        if not cap.isOpened():
+            print(f"Failed to open video file {file_path}")
+            exit(1)
+
+    try:
         while True:
-            ret, frame = self.cap.read()
+            ret, frame = cap.read()
+
             if not ret:
-                print("Error: Could not read frame.")
-                continue
-
-            cv2.imshow('Video Feed', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Reached the end of the video or there was an error.")
                 break
+
+            # Display the current frame
+            cv2.imshow('Video', frame)
+
+            # Exit the loop when 'q' is pressed
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        pass
+    except Exception as error:
+        print(f"None KeyboardInterrupt Error (Error: {error})")
+    finally:
+        # Release the VideoCapture object
+        cap.release()
+        # Close all OpenCV windows
+        cv2.destroyAllWindows()
 
 
 def main():
-    parse = argparse.ArgumentParser()
-    parse.add_argument('--camera-id', type=int, default=get_default_camera_id(), help='Camera ID')
-    parse.add_argument('--width', type=float, default=640, help='Live feed screen ratio width')
-    parse.add_argument('--height', type=float, default=480, help='Live feed screen ratio height')
-    parse.add_argument('--cut-video', type=int, default=10, help='Video size (in seconds)')
-    args = parse.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--camera-id', type=int, default=0, help='Camera ID')
+    parser.add_argument('--width', type=float, default=640, help='Live feed screen ratio width')
+    parser.add_argument('--height', type=float, default=480, help='Live feed screen ratio height')
+    parser.add_argument('--cut-video', type=int, default=10, help='Video size (in seconds)')
+    args = parser.parse_args()
 
     video_recorder = VideoRecorder(camera_id=args.camera_id, width=args.width, height=args.height, wait_time=args.cut_video)
     video_recorder.start_recording()
 
     try:
         while True:
-            command = input("Command (o to open feed, q to quit, h to update height, w to update width): ").strip()
-            if command == 'o':
-                video_recorder.display_feed(height=args.height, width=args.width)
-            if command == 'q':  # Quit
+            command = input("Command (q to quit): ").strip()
+            if command == 'q':
                 break
-            elif command == 'h':  # Update height
-                args.height = float(input("Updated height: "))
-            elif command == 'w':  # Update width
-                args.width = float(input("Updated width: "))
             else:
                 print(f"Invalid option {command}")
     except KeyboardInterrupt:
         print("\nInterrupted by user. Stopping...")
-    except Exception as error:
-        print(f"\nError: {error}")
     finally:
         video_recorder.stop_recording()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
+    # video_reader(file_path=os.path.join(BLOBS_DIR, '2024_05_28_08_36_27.mp4'))
---
+++ todo.md
@@ -0,0 +1,4 @@
+1. Record video and store every X seconds into a file <-- done 
+2. Open video to view <-- done 
+3. ML on Video - ML results should be JSON format 
+4. merge results
