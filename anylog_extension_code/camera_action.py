1804a06f65b995f2fc771e0420b1b397ba19b3f1
Ori Shadmon <oshadmon@gmail.com>
2024-05-31 13:17:18 -0700

    in progreses - write directly to file or AnyLog
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- anylog_extension_code/camera_action.py
+++ anylog_extension_code/camera_action.py
@@ -1,10 +1,12 @@
 import argparse
 import cv2
 import datetime
+import json
+import numpy
 import os
+import requests
 import time
 import threading
-import json
 
 ROOT_PATH = os.path.dirname(__file__).split("anylog_extension_code")[0]
 BLOBS_DIR = os.path.join(ROOT_PATH, 'blobs')
@@ -17,10 +19,88 @@ def get_default_camera_id():
             return i
     raise Exception("Error: Could not find an available camera.")
 
+def frames_to_video(frames, output_file, fps):
+    """
+    Convert a list of frames to a video file.
+    :args:
+        frames (list): List of frames (each frame is a list of lists).
+        output_file (str): Output video file path.
+        fps (float): Frames per second.
+    """
+    # Convert frames from lists back to numpy arrays
+    frames = [numpy.array(frame, dtype=numpy.uint8) for frame in frames]
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4')  # Codec for mp4
+    out = cv2.VideoWriter(output_file, fourcc, fps)
+
+    for frame in frames:
+        out.write(frame)
+
+    out.release()
+def write_metadata(blobs_dir:str=BLOBS_DIR, metadata:dict={}):
+    """
+    Write metadata to file
+    :args:
+        blobs_dir:str - dir to store file
+        metadata:dict - content to store in file
+    :paraams:
+        metadata_file:sr - JSON file to store data in
+    """
+    metadata_file = os.path.join(blobs_dir, 'metadata.json')
+
+    if not os.path.isfile(metadata_file):
+        open(metadata_file, 'w').close()
+
+    frames_to_video(frams=metadata['frames'],
+                    output_file=os.path.join(blobs_dir, metadata['file_name']),
+                    fps=metadata['fps'])
+
+    try:
+        with open(metadata_file, 'a') as f:
+            f.write(f"{json.dumps(metadata)}\n")
+    except Exception as error:
+        print(f"Failed to write metadata into file {metadata_file} (Error: {error})")
+
+def send_data(rest_conn:str, topic:str='livefeed', metadata:dict={}):
+    """
+    Send data into AnyLog / EdgeLake
+    :args:
+        rest_conn:str - REST connection information
+        topic:str - REST POST topic
+        metadata:dict - conteent to send into AnyLog/EdgeLake
+    :params:
+        auth:tuple - REST authentication
+        headers:dict - REST headers
+    """
+    auth = ()
+    conn = rest_conn
+    if '@' in rest_conn:
+        auth, conn = rest_conn.split('@')
+        auth = tuple(auth.split(':'))
+
+    headers = {
+        'command': 'data',
+        'topic': topic,
+        'User-Agent': 'AnyLog/1.23',
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        response = requests.post(url=f"http://{conn}", headers=headers, json=metadata, auth=auth, timeout=30)
+        response.raise_for_status()
+    except Exception as error:
+        print(f"Failed to send metadata to {conn} (Error: {error})")
+
+
 class VideoRecorder:
-    def __init__(self, blobs_dir:str, metadata_file:str, camera_id:int=0, width:float=640, height:float=480, wait_time:int=60):
+    def __init__(self, blobs_dir:str, db_name:str, table_name:str=None, rest_conn:str=None, rest_topic:str='livefeed',
+                 camera_id:int=0, width:float=640, height:float=480, wait_time:int=60):
         self.blobs_dir = blobs_dir
-        self.metadata_file = metadata_file
+        self.db_name = db_name
+        self.table_name = table_name
+        self.rest_conn = rest_conn
+        self.rest_topic = rest_topic
         self.camera_id = camera_id
         self.width = width
         self.height = height
@@ -57,7 +137,7 @@ class VideoRecorder:
         self.is_running.set()
         threading.Thread(target=self.__record).start()
 
-    def __record(self):
+    def __record_old(self):
         while self.is_running.is_set():
             ret, frame = self.cap.read()
             if not ret:
@@ -68,15 +148,47 @@ class VideoRecorder:
             if current_time - self.start_time >= self.wait_time:
                 self.video_writer.release()
                 self.video_writer = self.__create_video_writer()
-                metadata = {
-                    "file_path": self.filename,
-                    "start_time": datetime.datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
-                    "end_time": datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
-                    "duration": round(current_time - self.start_time, 2)
-                }
-                with open(self.metadata_file, 'a') as f:
-                    f.write(f"{json.dumps(metadata)}\n")
+
                 self.start_time = current_time
+            self.video_writer.write(frame)
+
+    def __record(self):
+        frames = []
+        while self.is_running.is_set():
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Error: Could not read frame.")
+                self.is_running.clear()
+                break
+            current_time = time.time()
+            frames.append(frame.tolist())
+            if current_time - self.start_time >= self.wait_time:
+                self.video_writer.release()
+                self.video_writer = self.__create_video_writer()
+                metadata = {
+                    "dbms": self.db_name,
+                    "table": self.table_name,
+                    "file_name": os.path.basename(self.filename),
+                    "readings": {
+                        "start_time": datetime.datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
+                        "end_time": datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S.%f'),
+                        "duration": round(current_time - self.start_time, 2)
+                    },
+                    "frame_count": len(frames),
+                    "fps": self.cap.get(cv2.CAP_PROP_FPS),
+                    "frames": frames
+                }
+
+                if self.table_name is None:
+                    metadata['table'] = os.path.basename(self.filename).split(".")[0]
+
+                if self.rest_conn:
+                    send_data(rest_conn=self.rest_conn, topic=self.rest_topic, metadata=metadata)
+                else:
+                    write_metadata(blobs_dir=self.blobs_dir, metadata=metadata)
+
+                self.start_time = current_time
+                frames = []
             self.video_writer.write(frame)
 
     def stop_recording(self):
@@ -107,12 +219,9 @@ def main():
 
     args.blobs_dir = os.path.expanduser(os.path.expandvars(args.blobs_dir))
     os.makedirs(args.blobs_dir, exist_ok=True)
-    metadata_file = os.path.join(args.blobs_dir, 'metadata.json')
-    if not os.path.isfile(metadata_file):
-        open(metadata_file, 'w').close()
+    # metadata_file = os.path.join(args.blobs_dir,
 
-    video_recorder = VideoRecorder(blobs_dir=args.blobs_dir, metadata_file=metadata_file, camera_id=args.camera_id,
-                                   width=args.width, height=args.height, wait_time=args.cut_video)
+    video_recorder = VideoRecorder(blobs_dir=args.blobs_dir, db_name='test', camera_id=args.camera_id, width=args.width, height=args.height, wait_time=args.cut_video)
     video_recorder.start_recording()
 
     try:
