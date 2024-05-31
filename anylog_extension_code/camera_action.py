2f4eabdaf52fced68b18e56ab204474016cc3d53
Ori Shadmon <oshadmon@gmail.com>
2024-05-31 13:32:05 -0700

    livefeed to file - working
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- anylog_extension_code/camera_action.py
+++ anylog_extension_code/camera_action.py
@@ -2,7 +2,7 @@ import argparse
 import cv2
 import datetime
 import json
-import numpy
+import numpy as np
 import os
 import requests
 import time
@@ -20,41 +20,19 @@ def get_default_camera_id():
     raise Exception("Error: Could not find an available camera.")
 
 def frames_to_video(frames, output_file, fps):
-    """
-    Convert a list of frames to a video file.
-    :args:
-        frames (list): List of frames (each frame is a list of lists).
-        output_file (str): Output video file path.
-        fps (float): Frames per second.
-    """
-    # Convert frames from lists back to numpy arrays
-    frames = [numpy.array(frame, dtype=numpy.uint8) for frame in frames]
-
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'mp4')  # Codec for mp4
-    out = cv2.VideoWriter(output_file, fourcc, fps)
-
+    frames = [np.array(frame, dtype=np.uint8) for frame in frames]
+    height, width, layers = frames[0].shape
+    size = (width, height)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_file, fourcc, fps, size)
     for frame in frames:
         out.write(frame)
-
     out.release()
-def write_metadata(blobs_dir:str=BLOBS_DIR, metadata:dict={}):
-    """
-    Write metadata to file
-    :args:
-        blobs_dir:str - dir to store file
-        metadata:dict - content to store in file
-    :paraams:
-        metadata_file:sr - JSON file to store data in
-    """
-    metadata_file = os.path.join(blobs_dir, 'metadata.json')
 
+def write_metadata(blobs_dir=BLOBS_DIR, metadata={}):
+    metadata_file = os.path.join(blobs_dir, 'metadata.json')
     if not os.path.isfile(metadata_file):
         open(metadata_file, 'w').close()
-
-    frames_to_video(frams=metadata['frames'],
-                    output_file=os.path.join(blobs_dir, metadata['file_name']),
-                    fps=metadata['fps'])
 
     try:
         with open(metadata_file, 'a') as f:
@@ -62,40 +40,27 @@ def write_metadata(blobs_dir:str=BLOBS_DIR, metadata:dict={}):
     except Exception as error:
         print(f"Failed to write metadata into file {metadata_file} (Error: {error})")
 
-def send_data(rest_conn:str, topic:str='livefeed', metadata:dict={}):
-    """
-    Send data into AnyLog / EdgeLake
-    :args:
-        rest_conn:str - REST connection information
-        topic:str - REST POST topic
-        metadata:dict - conteent to send into AnyLog/EdgeLake
-    :params:
-        auth:tuple - REST authentication
-        headers:dict - REST headers
-    """
+def send_data(rest_conn, topic='livefeed', metadata={}):
     auth = ()
     conn = rest_conn
     if '@' in rest_conn:
         auth, conn = rest_conn.split('@')
         auth = tuple(auth.split(':'))
-
     headers = {
         'command': 'data',
         'topic': topic,
         'User-Agent': 'AnyLog/1.23',
         'Content-Type': 'application/json'
     }
-
     try:
         response = requests.post(url=f"http://{conn}", headers=headers, json=metadata, auth=auth, timeout=30)
         response.raise_for_status()
     except Exception as error:
         print(f"Failed to send metadata to {conn} (Error: {error})")
 
-
 class VideoRecorder:
-    def __init__(self, blobs_dir:str, db_name:str, table_name:str=None, rest_conn:str=None, rest_topic:str='livefeed',
-                 camera_id:int=0, width:float=640, height:float=480, wait_time:int=60):
+    def __init__(self, blobs_dir, db_name, table_name=None, rest_conn=None, rest_topic='livefeed',
+                 camera_id=0, width=640, height=480, wait_time=60):
         self.blobs_dir = blobs_dir
         self.db_name = db_name
         self.table_name = table_name
@@ -109,7 +74,6 @@ class VideoRecorder:
         self.cap = self.__enable_video_capture()
         self.video_writer = self.__create_video_writer()
         self.start_time = time.time()
-
         if not self.cap.isOpened():
             raise Exception(f"Error: Could not open video device for camera ID {camera_id}")
 
@@ -119,7 +83,7 @@ class VideoRecorder:
             raise Exception(f"Failed to start video capture with camera {self.camera_id}")
         return cap
 
-    def __set_cap_size(self, height:float=None, width:float=None):
+    def __set_cap_size(self, height=None, width=None):
         if height is None:
             height = self.height
         if width is None:
@@ -137,21 +101,6 @@ class VideoRecorder:
         self.is_running.set()
         threading.Thread(target=self.__record).start()
 
-    def __record_old(self):
-        while self.is_running.is_set():
-            ret, frame = self.cap.read()
-            if not ret:
-                print("Error: Could not read frame.")
-                self.is_running.clear()
-                break
-            current_time = time.time()
-            if current_time - self.start_time >= self.wait_time:
-                self.video_writer.release()
-                self.video_writer = self.__create_video_writer()
-
-                self.start_time = current_time
-            self.video_writer.write(frame)
-
     def __record(self):
         frames = []
         while self.is_running.is_set():
@@ -161,7 +110,7 @@ class VideoRecorder:
                 self.is_running.clear()
                 break
             current_time = time.time()
-            frames.append(frame.tolist())
+            frames.append(frame)
             if current_time - self.start_time >= self.wait_time:
                 self.video_writer.release()
                 self.video_writer = self.__create_video_writer()
@@ -175,18 +124,15 @@ class VideoRecorder:
                         "duration": round(current_time - self.start_time, 2)
                     },
                     "frame_count": len(frames),
-                    "fps": self.cap.get(cv2.CAP_PROP_FPS),
-                    "frames": frames
+                    "fps": self.cap.get(cv2.CAP_PROP_FPS)
                 }
-
+                frames_to_video(frames, os.path.join(self.blobs_dir, metadata['file_name']), metadata['fps'])
                 if self.table_name is None:
                     metadata['table'] = os.path.basename(self.filename).split(".")[0]
-
                 if self.rest_conn:
                     send_data(rest_conn=self.rest_conn, topic=self.rest_topic, metadata=metadata)
                 else:
                     write_metadata(blobs_dir=self.blobs_dir, metadata=metadata)
-
                 self.start_time = current_time
                 frames = []
             self.video_writer.write(frame)
@@ -196,7 +142,7 @@ class VideoRecorder:
         self.cap.release()
         self.video_writer.release()
 
-    def display_feed(self, height:float=None, width:float=None):
+    def display_feed(self, height=None, width=None):
         self.__set_cap_size(height=height, width=width)
         while True:
             ret, frame = self.cap.read()
@@ -209,17 +155,16 @@ class VideoRecorder:
         cv2.destroyAllWindows()
 
 def main():
-    parse = argparse.ArgumentParser()
-    parse.add_argument('--camera-id', type=int, default=get_default_camera_id(), help='Camera ID')
-    parse.add_argument('--width', type=float, default=640, help='Live feed screen ratio width')
-    parse.add_argument('--height', type=float, default=480, help='Live feed screen ratio height')
-    parse.add_argument('--cut-video', type=int, default=10, help='Video size (in seconds)')
-    parse.add_argument('--blobs-dir', type=str, default=BLOBS_DIR, help='Directory to store videos cuts and insight')
-    args = parse.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--camera-id', type=int, default=get_default_camera_id(), help='Camera ID')
+    parser.add_argument('--width', type=float, default=640, help='Live feed screen ratio width')
+    parser.add_argument('--height', type=float, default=480, help='Live feed screen ratio height')
+    parser.add_argument('--cut-video', type=int, default=10, help='Video size (in seconds)')
+    parser.add_argument('--blobs-dir', type=str, default=BLOBS_DIR, help='Directory to store videos cuts and insight')
+    args = parser.parse_args()
 
     args.blobs_dir = os.path.expanduser(os.path.expandvars(args.blobs_dir))
     os.makedirs(args.blobs_dir, exist_ok=True)
-    # metadata_file = os.path.join(args.blobs_dir,
 
     video_recorder = VideoRecorder(blobs_dir=args.blobs_dir, db_name='test', camera_id=args.camera_id, width=args.width, height=args.height, wait_time=args.cut_video)
     video_recorder.start_recording()
